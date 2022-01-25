6668217b2c154a82ac112d56878c2b025dab8919
Agnija <ilzena.agnija@gmail.com>
2022-01-25 15:25:59 +0100

    added entity Galery, Photo; migrations

---
+++ src/Entity/Photo.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PhotoRepository::class)
+ */
+class Photo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $link;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Galery::class, inversedBy="photos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private Galery $galery;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getGalery(): ?Galery
+    {
+        return $this->galery;
+    }
+
+    public function setGalery(?Galery $galery): self
+    {
+        $this->galery = $galery;
+
+        return $this;
+    }
+}
