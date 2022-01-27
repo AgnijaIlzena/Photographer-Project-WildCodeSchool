2bb3fb246729dac7171e947c369fbb6924813e53
N-akademy <56726632+N-akademy@users.noreply.github.com>
2022-01-27 12:14:24 +0100

    'correction+controller'

--- src/Controller/GaleryController.php
+++ src/Controller/GaleryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Galery;
+use App\Entity\Photo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,9 +39,32 @@ class GaleryController extends AbstractController
 
         $photos = $galery->getPhotos();
 
-
         return $this->render('galery/show.html.twig', [
             'galery' => $galery, 'photos' => $photos
+        ]);
+    }
+
+    /**
+     * @Route("/galery/{galeryId}/photo/{photoId}", requirements={"id"="\d+"}, methods={"GET"}, name="galery_photo_show")
+     */
+    public function showOne(int $galeryId, int $photoId): Response
+    {
+        $galery = $this->getDoctrine()
+            ->getRepository(Galery::class)
+            ->findOneBy(['id' => $galeryId]);
+
+        if (!$galery) {
+            throw $this->createNotFoundException(
+                'No galery with id : ' . $galeryId . ' found in galeries table.'
+            );
+        }
+
+        $photo = $this->getDoctrine()
+            ->getRepository(Photo::class)
+            ->findOneBy(['id' => $photoId]);
+
+        return $this->render('galery/photo_show.html.twig', [
+            'galery' => $galery, 'photo' => $photo
         ]);
     }
 }
