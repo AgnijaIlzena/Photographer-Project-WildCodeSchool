ec39cebc4303dddca7cfc65a5f19a2b13f51009c
TchengSG <tcheng.siong@outlook.com>
2022-01-31 16:32:55 +0100

    carousel non fonctionnel 3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function createCarousel(): Response
     {
-        return $this->render('home/index.html.twig');
+        $carousel = $this->getDoctrine()
+            ->getRepository(Photo::class)
+            ->findAll();
+
+        return $this->render(
+            'home/index.html.twig',
+            ['carousel' => $carousel]
+        );
     }
 }
