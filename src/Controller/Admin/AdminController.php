f6610875686e6993f770581874ca935f06d93854
Agnija <ilzena.agnija@gmail.com>
2022-02-01 22:18:20 +0100

    admin role integrated

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Form\AdminLoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminController extends AbstractController
 {
@@ -16,5 +19,29 @@ class AdminController extends AbstractController
         return $this->render('adminDashboard/admin/index.html.twig', [
             'dashboard' => 'Admin dashboard',
         ]);
+    }
+
+    /**
+     * @Route ("/connect", name="connect")
+     */
+    public function connectAdmin(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('connect/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route ("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
     }
 }
